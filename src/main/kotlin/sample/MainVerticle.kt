b6e0ecae88b9a0e7b32acbf9f28ac26365bb7246
Thomas Segismont <tsegismo@redhat.com>
2017-09-22 18:01:52 +0200

    Initial commit

---
+++ src/main/kotlin/sample/MainVerticle.kt
@@ -0,0 +1,13 @@
+package sample
+
+import io.vertx.core.AbstractVerticle
+
+/**
+ * @author Thomas Segismont
+ */
+class MainVerticle: AbstractVerticle() {
+
+    override fun start() {
+        println("bimbo!!!!!")
+    }
+}
