c31dd92ab6331c8433fb18f8486e23a3e2d4ee2a
Thomas Segismont <tsegismo@redhat.com>
2019-06-19 14:46:16 +0200

    Use Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin("jvm") version "1.3.31"
+  id("io.vertx.vertx-plugin") version "0.6.0"
+}
+
+repositories {
+  jcenter()
+}
+
+dependencies {
+  implementation("io.vertx:vertx-lang-kotlin")
+  implementation("io.vertx:vertx-rx-java2")
+  implementation("io.vertx:vertx-jdbc-client")
+  implementation("io.vertx:vertx-web")
+  implementation("com.h2database:h2:1.4.196")
+  implementation(kotlin("stdlib-jdk8"))
+}
+
+vertx {
+  vertxVersion = "3.5.1"
+  mainVerticle = "sample.MainVerticle"
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.wrapper {
+  gradleVersion = "5.2"
+}
