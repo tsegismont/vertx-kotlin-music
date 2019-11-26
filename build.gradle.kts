18855dd55d03e12bfdaee2ffa0638dbf18e248a4
Thomas Segismont <tsegismo@redhat.com>
2019-11-26 16:07:04 +0100

    Upgrade Gradle Vertx Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.3.60"
-  id("io.vertx.vertx-plugin") version "0.6.0"
+  id("io.vertx.vertx-plugin") version "1.0.1"
 }
 
 repositories {
